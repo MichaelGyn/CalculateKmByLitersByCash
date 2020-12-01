18dafc2a33c0dddda6c231b9caf332d9861804e9
michael lucas <michaellucas360@gmail.com>
2020-12-01 17:48:44 -0300

    ORG_RESPONSABILIDADES

--- index.php
+++ index.php
@@ -42,39 +42,23 @@
   <div class="row  text-success d-flex justify-content-center">
     <h2>Menu Principal</h2>
   </div>
-  <div class="form-group text-info d-flex justify-content-center">
-  <form action="" method="POST">
+  <div class="row  text-info d-flex justify-content-center">
+    <h2>Escolha uma das ferramentas abaixo:</h2>
+  </div>
+  <div class="container">
   <div class="row">
     <div class="col-sm">
-      <h2>Combustível: Álcool</h2>
-        <label for="Distance1">Distância a calcular: </label>
-        <input type="text" class="border border-primary" name="distance1" id="distance1" onkeyup="formatCoin(this);" placeholder="Em Km" autofocus required><br>
-        <label for="kmPerLiter1">Quantos km seu carro faz com 1 litro? </label>
-        <input type="text" class="border border-primary" name="kmPerLiter1" id="kmPerLiter1" onkeyup="formatCoin(this);" placeholder="Em km" required><br>
-        <label for="FuelPrice1">Qual o preço do litro de combustível? </label>
-        <input type="text" class="border border-primary" name="FuelPrice1" id="FuelPrice1" onkeyup="formatCoin(this);" placeholder="Em R$" required><br>
+      <a class="btn btn-outline-primary" href="/tools/calculateKmByLitersByCash" data-toggle="tooltip" title="Calcula a distância informada pela consumo do seu veículo, em até 2 tipos de combustíveis.">Ferramenta Distância/Combustível</a>
     </div>
     <div class="col-sm">
-      <h2>Combustível: Gasolina</h2>
-      <label for="Distance2">Distância a calcular: </label>
-        <input type="text" class="border border-primary" name="distance2" id="distance2" onkeyup="formatCoin(this);" placeholder="Em Km" required><br>
-        <label for="kmPerLiter2">Quantos km seu carro faz com 1 litro? </label>
-        <input type="text" class="border border-primary" name="kmPerLiter2" id="kmPerLiter2" onkeyup="formatCoin(this);" placeholder="Em km" required><br>
-        <label for="FuelPrice2">Qual o preço do litro de combustível? </label>
-        <input type="text" class="border border-primary" name="FuelPrice2" id="FuelPrice2" onkeyup="formatCoin(this);" placeholder="Em R$" required><br>
+      <a class="btn btn-outline-primary" href="#" data-toggle="tooltip" title="Disabled">Disabled</a>
+    </div>
+    <div class="col-sm">
+    <a class="btn btn-outline-primary" href="#" data-toggle="tooltip" title="Disabled" disabled>Disabled</a>
     </div>
   </div>
-  <div class="row">
-    <div class="col-sm d-flex justify-content-center">
-    <button type="submit" id="Enviar" class="btn btn-outline-success" data-toggle="modal" data-target="#exampleModal">Enviar</button>
-    </div>
-    
-    <div class="col-sm d-flex justify-content-center">
-      <input type="reset" class="btn btn-outline-danger" value="Limpar">
-    </div>
-  </div>
-</form>
 </div>
+
 </body>
 
 
@@ -88,75 +72,4 @@
 
 <?php
 
-if(isset($_POST['distance1']) && !empty($_POST['distance1']) && isset($_POST['kmPerLiter1']) && !empty($_POST['kmPerLiter1']) && isset($_POST['FuelPrice1']) && !empty($_POST['FuelPrice1']) && isset($_POST['distance2']) && !empty($_POST['distance2']) && isset($_POST['kmPerLiter2']) && !empty($_POST['kmPerLiter2']) && isset($_POST['FuelPrice2']) && !empty($_POST['FuelPrice2'])) {
-    $distance1 = filter_input(INPUT_POST, 'distance1');
-        } else {
-            die();
-            }
-
-$kmPerLiter1 = filter_input(INPUT_POST, 'kmPerLiter1');
-$FuelPrice1 = filter_input(INPUT_POST, 'FuelPrice1');
-$distance2 = filter_input(INPUT_POST, 'distance2');
-$kmPerLiter2 = filter_input(INPUT_POST, 'kmPerLiter2');
-$FuelPrice2 = filter_input(INPUT_POST, 'FuelPrice2');
-
-require_once('validations.php');
-
-$distance1 = convertToFlat($distance1);
-$kmPerLiter1 = convertToFlat($kmPerLiter1);
-$FuelPrice1 = convertToFlat($FuelPrice1);
-$distance2 = convertToFlat($distance2);
-$kmPerLiter2 = convertToFlat($kmPerLiter2);
-$FuelPrice2 = convertToFlat($FuelPrice2);
-
-$totalLiters1 = $distance1 / $kmPerLiter1;
-$result1 = $totalLiters1 * $FuelPrice1;
-$result1 = round($result1, 2);
-$result1 = number_format($result1, 2, ',', '.');
-//$result1 = str_ireplace('.', ',', $result1);
-
-
-
-$totalLiters2 = $distance2 / $kmPerLiter2;
-$result2 = $totalLiters2 * $FuelPrice2;
-$result2 = round($result2, 2);
-$result2 = number_format($result2, 2, ',', '.');
-//$result2 = str_ireplace('.', ',', $result2);
-
-
-if (!empty($result1)) { ?>
-
-<!--modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Resultado da Busca:</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p><?php 
-            echo "<h2>Abastecido com Álcool</h2>";
-            echo "Em uma distância percorrida de " . $distance1 . " quilômetros, você gastará um total de R$ " . $result1 . " reais, pois seu carro atualmente faz " . $kmPerLiter1 . " km/l, abastecido com álcool";
-            echo '<br>';
-            echo '<br>';
-            echo "<h2>Abastecido com Gasolina</h2>";
-            echo "Em uma distância percorrida de " . $distance2 . " quilômetros, você gastará um total de R$ " . $result2 . " reais, pois seu carro atualmente faz " . $kmPerLiter2 . " km/l, abastecido com gasolina";
-          ?></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" href="/"class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-      </div>
-    </div>
-  </div>
-</div>
-<script>
-$('#exampleModal').modal('show');
-</script>
- 
-<?php } else {
-  echo "<script>alert('0 Resultados encontrados. Favor pesquise novamente');</script>";
-  header('location: index.php');
-} ?>
+ ?>
--- tools/calculateKmByLitersByCash/result.php
+++ tools/calculateKmByLitersByCash/result.php
@@ -2,8 +2,8 @@
 
 if(isset($_POST['distanceMenuAlcohol']) && !empty($_POST['distanceMenuAlcohol']) && isset($_POST['kmPerLiterMenuAlcohol']) && !empty($_POST['kmPerLiterMenuAlcohol']) && isset($_POST['fuelPriceMenuAlcohol']) && !empty($_POST['fuelPriceMenuAlcohol']) && isset($_POST['distanceMenuGasoline']) && !empty($_POST['distanceMenuGasoline']) && isset($_POST['kmPerLiterMenuGasoline']) && !empty($_POST['kmPerLiterMenuGasoline']) && isset($_POST['fuelPriceMenuGasoline']) && !empty($_POST['fuelPriceMenuGasoline'])) {
     $distanceMenuAlcohol = filter_input(INPUT_POST, 'distanceMenuAlcohol');
-        } else {
-            die("teste");
+     } else {
+            die("No isset");
             }
 
 
@@ -28,14 +28,37 @@ echo '<br>';
 echo $fuelPriceMenuGasoline;
 echo '<br>';
 */
-require_once('validations.php');
 
-$distanceMenuAlcohol = convertToFlat($distanceMenuAlcohol);
-$kmPerLiterMenuAlcohol = convertToFlat($kmPerLiterMenuAlcohol);
-$fuelPriceMenuAlcohol = convertToFlat($fuelPriceMenuAlcohol);
-$distanceMenuGasoline = convertToFlat($distanceMenuGasoline);
-$kmPerLiterMenuGasoline = convertToFlat($kmPerLiterMenuGasoline);
-$fuelPriceMenuGasoline = convertToFlat($fuelPriceMenuGasoline);
+//require_once './processement.php';
+
+
+function convertToFloat($parameter) {
+
+  $contator = strlen($parameter);
+  $casaDosMilhoes = 14;
+  
+  if(strlen($contator > $casaDosMilhoes)) {
+      echo "<script>alert('Essa ferramenta calcula dados até milhões! Favor inserir um número até na casa dos milhões');</script>";
+      header('location: index.php');
+      die();
+  } else{
+  
+  $result = str_ireplace('.', '', $parameter);
+  $result = str_ireplace(',', '.', $result);
+  $parameter = floatval($result);
+
+  return $parameter;
+  
+      }
+  }
+
+
+$distanceMenuAlcohol = convertToFloat($distanceMenuAlcohol);
+$kmPerLiterMenuAlcohol = convertToFloat($kmPerLiterMenuAlcohol);
+$fuelPriceMenuAlcohol = convertToFloat($fuelPriceMenuAlcohol);
+$distanceMenuGasoline = convertToFloat($distanceMenuGasoline);
+$kmPerLiterMenuGasoline = convertToFloat($kmPerLiterMenuGasoline);
+$fuelPriceMenuGasoline = convertToFloat($fuelPriceMenuGasoline);
 
 $totalLitersMenuAlcohol = $distanceMenuAlcohol / $kmPerLiterMenuAlcohol;
 $resultMenuAlcohol = $totalLitersMenuAlcohol * $fuelPriceMenuAlcohol;
@@ -43,15 +66,13 @@ $resultMenuAlcohol = round($resultMenuAlcohol, 2);
 $resultMenuAlcohol = number_format($resultMenuAlcohol, 2, ',', '.');
 //$resultMenuAlcohol = str_ireplace('.', ',', $resultMenuAlcohol);
 
-
-
 $totalLitersMenuGasoline = $distanceMenuGasoline / $kmPerLiterMenuGasoline;
 $resultMenuGasoline = $totalLitersMenuGasoline * $fuelPriceMenuGasoline;
 $resultMenuGasoline = round($resultMenuGasoline, 2);
 $resultMenuGasoline = number_format($resultMenuGasoline, 2, ',', '.');
 //$resultMenuGasoline = str_ireplace('.', ',', $resultMenuGasoline);
 
-if (!empty($resultMenuAlcohol)) { require_once '/views/pop-up.html';?>
+if (!empty($resultMenuAlcohol)) { ;?>
 
     <!--modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
@@ -66,11 +87,11 @@ if (!empty($resultMenuAlcohol)) { require_once '/views/pop-up.html';?>
           <div class="modal-body">
             <p><?php 
                 echo "<h2>Abastecido com Álcool</h2>";
-                echo "Em uma distância percorrida de " . $distance1 . " quilômetros, você gastará um total de R$ " . $result1 . " reais, pois seu carro atualmente faz " . $kmPerLiter1 . " km/l, abastecido com álcool";
+                echo "Em uma distância percorrida de " . $distanceMenuAlcohol . " quilômetros, você gastará um total de R$ " . $resultMenuAlcohol . " reais, pois seu carro atualmente faz " . $kmPerLiterMenuAlcohol . " km/l, abastecido com álcool";
                 echo '<br>';
                 echo '<br>';
                 echo "<h2>Abastecido com Gasolina</h2>";
-                echo "Em uma distância percorrida de " . $distance2 . " quilômetros, você gastará um total de R$ " . $result2 . " reais, pois seu carro atualmente faz " . $kmPerLiter2 . " km/l, abastecido com gasolina";
+                echo "Em uma distância percorrida de " . $distanceMenuGasoline . " quilômetros, você gastará um total de R$ " . $resultMenuGasoline . " reais, pois seu carro atualmente faz " . $kmPerLiterMenuGasoline . " km/l, abastecido com gasolina";
               ?></p>
           </div>
           <div class="modal-footer">
@@ -86,4 +107,4 @@ if (!empty($resultMenuAlcohol)) { require_once '/views/pop-up.html';?>
     <?php } else {
       echo "<script>alert('0 Resultados encontrados. Favor pesquise novamente');</script>";
       header('location: index.php');
-    } ?>
+    }  ?>
