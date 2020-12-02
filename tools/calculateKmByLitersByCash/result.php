1cbed1346c4b3ff6422d102198cf2e7e2326f4ec
michael lucas <michaellucas360@gmail.com>
2020-12-02 11:08:03 -0300

    RESOLVENDO_ERRO_NO_REQUIRE_ONCE

--- tools/calculateKmByLitersByCash/result.php
+++ tools/calculateKmByLitersByCash/result.php
@@ -29,29 +29,7 @@ echo $fuelPriceMenuGasoline;
 echo '<br>';
 */
 
-//require_once './processement.php';
-
-
-function convertToFloat($parameter) {
-
-  $contator = strlen($parameter);
-  $casaDosMilhoes = 14;
-  
-  if(strlen($contator > $casaDosMilhoes)) {
-      echo "<script>alert('Essa ferramenta calcula dados até milhões! Favor inserir um número até na casa dos milhões');</script>";
-      header('location: index.php');
-      die();
-  } else{
-  
-  $result = str_ireplace('.', '', $parameter);
-  $result = str_ireplace(',', '.', $result);
-  $parameter = floatval($result);
-
-  return $parameter;
-  
-      }
-  }
-
+require_once 'processement.php';
 
 $distanceMenuAlcohol = convertToFloat($distanceMenuAlcohol);
 $kmPerLiterMenuAlcohol = convertToFloat($kmPerLiterMenuAlcohol);
